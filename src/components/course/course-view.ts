d2edcdde5573be83c28c036c15fcff66c9ee3a27
Eamonn de Leastar <edeleastar@wit.ie>
2019-11-26 06:41:59 +0000

    remove redundant approaches

--- src/components/course/course-view.ts
+++ src/components/course/course-view.ts
@@ -12,32 +12,7 @@ export class CourseView {
   course: Course;
   gridOptions: GridOptions;
 
-  rowData = [
-    //{
-    //   athlete: "Michael Phelps",
-    //   age: 23,
-    //   country: "United States",
-    //   year: 2008,
-    //   date: "24/08/2008",
-    //   sport: "Swimming",
-    //   gold: 8,
-    //   silver: 0,
-    //   bronze: 0,
-    //   total: 8
-    // },
-    // {
-    //   athlete: "Michael Phelps",
-    //   age: 19,
-    //   country: "United States",
-    //   year: 2004,
-    //   date: "29/08/2004",
-    //   sport: "Swimming",
-    //   gold: 6,
-    //   silver: 0,
-    //   bronze: 2,
-    //   total: 8
-    // }
-  ];
+  rowData = [];
 
   columnDefs = [
     { headerName: "Student", field: "student", width: 60, rowGroup: true, hide: true},
@@ -48,14 +23,6 @@ export class CourseView {
     { headerName: "Tile", field: "title", width: 150 },
     { headerName: "Date", field: "date", width: 100 },
     { headerName: "Count", field: "count", width: 50 }
-
-
-    // { headerName: "Student", field: "student", width: 60, rowGroup: true},
-    // { headerName: "Topic", field: "topic", width: 60, rowGroup: true },
-    // { headerName: "Element", field: "element", width: 60, rowGroup: true},
-    // { headerName: "Chapter", field: "chapter", width: 60, rowGroup: true },
-    // { headerName: "Item", field: "item", width: 60, rowGroup: true,},
-    // { headerName: "Count", field: "count", width: 60 }
 ];
   constructor(
     private courseRepo: CourseRepo,
@@ -65,30 +32,23 @@ export class CourseView {
     this.gridOptions = <GridOptions>{};
     this.gridOptions.rowData = this.rowData;
     this.gridOptions.columnDefs = this.columnDefs;
-    //this.gridOptions.treeData = true, // enable Tree Data mode
     this.gridOptions.animateRows = true;
-    //this.gridOptions.groupMultiAutoColumn = true
-    //this.gridOptions.groupHideOpenParents = true;
-    //this.gridOptions.groupMultiAutoColumn = true;
   }
 
   populateRows(userData) {
-    const root = {
-      student: userData.email,
-      course: userData.title
-    };
-    this.rowData.push({ student: root.student, title: userData.title, date: userData.last, count:userData.count  });
+    const student = userData.email;
+    this.rowData.push({ student: student, title: userData.title, date: userData.last, count:userData.count  });
     userData.los.forEach((value, index) => {
       let topic = value.id;
-      this.rowData.push({ student: root.student, topic: topic, title: value.title, date: value.last, count:value.count });
+      this.rowData.push({ student: student, topic: topic, title: value.title, date: value.last, count:value.count });
       value.los.forEach((value, index) => {
         let element = value.id;
-        this.rowData.push({student: root.student, topic: topic, element: element, title: value.title, date: value.last, count:value.count });
+        this.rowData.push({student: student, topic: topic, element: element, title: value.title, date: value.last, count:value.count });
         value.los.forEach((value, index) => {
           let chapter = value.id;
-          this.rowData.push({student: root.student, topic: topic, element: element, chapter: chapter, title: value.title, date: value.last, count:value.count });
+          this.rowData.push({student: student, topic: topic, element: element, chapter: chapter, title: value.title, date: value.last, count:value.count });
           value.los.forEach((value, index) => {
-            this.rowData.push({student: root.student, topic: topic, element: element, chapter: chapter, item: value.id, title: value.title, date: value.last, count:value.count});
+            this.rowData.push({student: student, topic: topic, element: element, chapter: chapter, item: value.id, title: value.title, date: value.last, count:value.count});
           });
         });
       });
