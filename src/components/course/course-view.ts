4837105cb022e0165bca2b3bab8c9754cd1ae64b
Eamonn de Leastar <edeleastar@wit.ie>
2019-11-26 14:19:09 +0000

    rework to shared grid

--- src/components/course/course-view.ts
+++ src/components/course/course-view.ts
@@ -4,24 +4,14 @@ import { CourseRepo } from "../../services/course-repo";
 import { NavigatorProperties } from "../../resources/elements/navigators/navigator-properties";
 import { AnalyticsService } from "../../services/analytics-service";
 import { autoinject } from "aurelia-framework";
-import { populateRows } from "../../services/grid-utils";
+import { UserGrid } from "../../services/user-grid";
 import "ag-grid-enterprise";
 
 @autoinject
 export class CourseView {
   course: Course;
   gridOptions: GridOptions;
-  rowData = [];
-  columnDefs = [
-    { headerName: "Student", field: "student", width: 60, rowGroup: true, hide: true },
-    { headerName: "Topic", field: "topic", width: 60, rowGroup: true, hide: true },
-    { headerName: "Element", field: "l1", width: 60, rowGroup: true, hide: true },
-    { headerName: "Chapter", field: "l2", width: 60, rowGroup: true, hide: true },
-    { headerName: "Item", field: "l3", width: 60, rowGroup: true, hide: true },
-    { headerName: "Title", field: "title", width: 150 },
-    { headerName: "Date", field: "date", width: 100 },
-    { headerName: "Count", field: "count", width: 50 }
-  ];
+  userGrid = new UserGrid();
 
   constructor(
     private courseRepo: CourseRepo,
@@ -29,24 +19,28 @@ export class CourseView {
     private analyticsService: AnalyticsService
   ) {
     this.gridOptions = <GridOptions>{};
-    this.gridOptions.rowData = this.rowData;
-    this.gridOptions.columnDefs = this.columnDefs;
+    this.gridOptions.rowData = this.userGrid.rowData;
+    this.gridOptions.columnDefs = this.userGrid.columnDefs;
+    this.gridOptions.defaultColDef = this.userGrid.defaultColDef;
+    this.gridOptions.autoGroupColumnDef = this.userGrid.autoGroupColumnDef;
     this.gridOptions.animateRows = true;
     this.gridOptions.groupRemoveLowestSingleChildren = true;
+    //this.gridOptions.groupRemoveSingleChildren=true;
+   // this.gridOptions.groupHideOpenParents=true;
   }
 
   async activate(params, route) {
     await this.courseRepo.fetchCourse(params.courseurl);
     this.course = this.courseRepo.course;
     this.navigatorProperties.init(this.course.lo);
-    let users = await this.analyticsService.getUsers(this.course);
-    let usersUsage = await this.analyticsService.getUserUsage(this.course, "edeleastar@wit*ie");
-    populateRows(usersUsage, this.rowData);
-    console.log(usersUsage);
+    const users = await this.analyticsService.getUsers(this.course);
+    for (let user of users) {
+      let usersUsage = await this.analyticsService.getUserUsage(this.course, user.email);
+      this.userGrid.populate(usersUsage);
+    }
   }
 
   private onReady(params) {
     params.api.sizeColumnsToFit();
-    //   params.api.autoSizeAllColumns();
   }
 }
---
+++ src/services/user-grid.ts
@@ -0,0 +1,67 @@
+export interface Row {
+  student: string;
+  title: string;
+  date: string;
+  count: string;
+  topic?: string;
+  l1?: string;
+  l2?: string;
+  l3?: string;
+}
+
+const loElements = ["topic", "l1", "l2", "l3"];
+
+function generateRow(student: string, lo, ...params) {
+  let row: Row = {
+    student: student,
+    title: lo.title,
+    date: lo.last,
+    count: lo.count
+  };
+  params.forEach((param, index) => {
+    row[loElements[index]] = param;
+  });
+  return row;
+}
+
+export class UserGrid {
+
+  rowData = [];
+  columnDefs = [
+    { headerName: "Student", field: "student", width: 20, rowGroup: true, hide: true},
+    { headerName: "Topic", field: "topic", width: 10, rowGroup: true, hide: true },
+    { headerName: "Element", field: "l1", width: 40, rowGroup: true, hide: true },
+    { headerName: "Chapter", field: "l2", width: 40, rowGroup: true, hide: true  },
+    { headerName: "Item", field: "l3", width: 40, rowGroup: false, hide: true },
+    { headerName: "Title", field: "title", width: 150 },
+    { headerName: "Date", field: "date", width: 100  },
+    { headerName: "Count", field: "count", width: 50  }
+  ];
+  defaultColDef = {
+    width: 80,
+    sortable: true,
+    resizable: true
+  };
+  autoGroupColumnDef = {
+    headerName: 'Student',
+    width: 100,
+    //field: 'student'
+  };
+
+  populate(userData) {
+    const student = userData.email;
+    this.rowData.push(generateRow(student, userData));
+    userData.los.forEach(topic => {
+      this.rowData.push(generateRow(student, topic, topic.id));
+      topic.los.forEach(l1 => {
+        this.rowData.push(generateRow(student, l1, topic.id, l1.id));
+        l1.los.forEach(l2 => {
+          this.rowData.push(generateRow(student, l2, topic.id, l1.id, l2.id));
+          l2.los.forEach(l3 => {
+            this.rowData.push(generateRow(student, l3, topic.id, l1.id, l2.id, l3.id));
+          });
+        });
+      });
+    });
+  }
+}
